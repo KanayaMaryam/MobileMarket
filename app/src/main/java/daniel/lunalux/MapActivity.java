994b5b35552c5261fd380bd8ae72732634970090
MrSnrhms <ziggywa@gmail.com>
2015-09-05 22:17:22 -0700

    Start Asynctask in the splash screen activity, removed some garbage

--- app/src/main/java/daniel/lunalux/MapActivity.java
+++ app/src/main/java/daniel/lunalux/MapActivity.java
@@ -3,7 +3,6 @@ package daniel.lunalux;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
-import android.os.AsyncTask;
 import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
@@ -26,23 +25,14 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.URL;
-import java.net.URLConnection;
-import java.util.Date;
-import java.util.TimeZone;
-import java.text.SimpleDateFormat;
 
 public class MapActivity extends AppCompatActivity implements LocationListener{
     public static double longitude;
     public static double latitude;
-    private GoogleMap map;
-    private LocationManager locationManager;
+    public static GoogleMap map;
+    public static JSONArray array;
+    public static LocationManager locationManager;
     //minimum time and distance delta for onLocationChange to be called
     private static final long MIN_TIME = 40;
     private static final float MIN_DISTANCE = 1000;
@@ -50,7 +40,7 @@ public class MapActivity extends AppCompatActivity implements LocationListener{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setTitle("Loading Seller Locations...");
+        setTitle("Seller Locations");
         setContentView(R.layout.activity_map);
 
         map = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
@@ -82,65 +72,21 @@ public class MapActivity extends AppCompatActivity implements LocationListener{
                 return info;
             }
         });
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME, MIN_DISTANCE, this);
-
-        //initially start camera at last known location
-        Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-        String url = "";
-        if (location!=null){
-            longitude = location.getLongitude();
-            latitude = location.getLatitude();
-            map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(latitude,longitude),15));
-            url = "http://yardsalebackendproduction.azurewebsites.net/api/YardSaleEvent?latitude="+latitude+"&longitude="+longitude+"&distance=1000";
-        } else { //default back to seattle
-            url = "http://yardsalebackendproduction.azurewebsites.net/api/YardSaleEvent?latitude="+47.6097+"&longitude="+-122.3331+"&distance=1000";
-        }
-        new LongOperation().execute(url);
-    }
-
-    //class that extends AsyncTask class. handles the server stuff outside the main thread
-    private class LongOperation  extends AsyncTask <String, Void, JSONArray> {
-        //this function retrieves the JSON data and passes it to onPostExecute in a JSONArray format
-        protected JSONArray doInBackground(String... urls) {
-            JSONArray array = null;
-            try{
-                URL url = new URL(urls[0]);
-                URLConnection connection = url.openConnection();
-
-                //in = new BufferedInputStream(connection.getInputStream());
-                BufferedReader streamReader = new BufferedReader(new InputStreamReader(connection.getInputStream(), "UTF-8"));
-                StringBuilder responseStrBuilder = new StringBuilder();
-                String inputStr;
-                while ((inputStr = streamReader.readLine()) != null) {
-                    responseStrBuilder.append(inputStr);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(latitude,longitude),15));
+        if (MapActivity.array != null){
+            try {
+                for (int i = 0; i < array.length(); i++) {
+                    YardSale obj = new YardSale((JSONObject)array.get(i));
+                    MapActivity.map.addMarker(new MarkerOptions().position(new LatLng(obj.getLocationLatitude(), obj.getLocationLongitude()))
+                            .title(obj.getAddress())
+                            .snippet("Phone: " + obj.getPhoneNumber()
+                                    + "\nStart Time: " + obj.startToLocal() +
+                                    "\nEnd Time: " + obj.endToLocal()));
                 }
-                streamReader.close();
-                String data = responseStrBuilder.toString();
-                array = new JSONArray(data);
-
-            } catch (Exception e){
-                e.printStackTrace();
-            }
-            return array;
+            } catch (Exception e) {e.printStackTrace();}
         }
 
-        //updates the map with the markers
-        protected void onPostExecute(JSONArray array){
-            if (array != null){
-                try {
-                    for (int i = 0; i < array.length(); i++) {
-                        YardSale obj = new YardSale((JSONObject)array.get(i));
-                        map.addMarker(new MarkerOptions().position(new LatLng(obj.getLocationLatitude(), obj.getLocationLongitude()))
-                                .title(obj.getAddress())
-                                .snippet("Phone: " + obj.getPhoneNumber()
-                                         + "\nStart Time: " + obj.startToLocal() +
-                                          "\nEnd Time: " + obj.endToLocal()));
-                    }
-                    setTitle("Seller Locations");
-                } catch (Exception e) {e.printStackTrace();}
-            }
-        }
     }
 
     @Override
@@ -152,8 +98,10 @@ public class MapActivity extends AppCompatActivity implements LocationListener{
         map.animateCamera(cameraUpdate);
         locationManager.removeUpdates(this);
         //show info window by default, places marker with current location and title "you are here!"
+        this.longitude = location.getLongitude();
+        this.latitude = location.getLatitude();
         map.addMarker(new MarkerOptions()
-                .position(new LatLng(location.getLatitude(), location.getLongitude()))
+                .position(new LatLng(latitude, longitude))
                 .title("You are here!")).showInfoWindow();
 
     }
--- app/src/main/java/daniel/lunalux/SplashScreenActivity.java
+++ app/src/main/java/daniel/lunalux/SplashScreenActivity.java
@@ -1,16 +1,31 @@
 package daniel.lunalux;
 
+import android.content.Context;
 import android.content.Intent;
 import android.app.Activity;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Point;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.RelativeLayout;
+import android.location.LocationManager;
+import android.location.Location;
+
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 
 
 public class SplashScreenActivity extends Activity {
@@ -19,30 +34,52 @@ public class SplashScreenActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        new LongOperation().execute();
+    }
 
-        Thread background = new Thread() {
-            public void run() {
-
-                try {
-                    // Thread will sleep for 1 seconds
-                    sleep(1000);
-
-                    // After 1 seconds redirect to another intent
-                    Intent i=new Intent(getBaseContext(),MapActivity.class);
-                    startActivity(i);
-
-                    //Remove activity
-                    finish();
-
-                } catch (Exception e) {
-
+    //class that extends AsyncTask class. handles the server stuff outside the main thread
+    private class LongOperation  extends AsyncTask<Void, Void, JSONArray> {
+        //this function retrieves the JSON data and passes it to onPostExecute in a JSONArray format
+        protected JSONArray doInBackground(Void... temp) {
+            JSONArray array = null;
+            try{
+                MapActivity.locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+                Location location = MapActivity.locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                String urlTemp = "";
+                if (location!=null) {
+                    MapActivity.longitude = location.getLongitude();
+                    MapActivity.latitude = location.getLatitude();
+                    urlTemp = "http://yardsalebackendproduction.azurewebsites.net/api/YardSaleEvent?latitude="+MapActivity.latitude+"&longitude="+MapActivity.longitude+"&distance=1000";
+                } else { //default back to seattle
+                    urlTemp = "http://yardsalebackendproduction.azurewebsites.net/api/YardSaleEvent?latitude="+47.6097+"&longitude="+-122.3331+"&distance=1000";
                 }
+                URL url = new URL(urlTemp);
+                URLConnection connection = url.openConnection();
+
+                //in = new BufferedInputStream(connection.getInputStream());
+                BufferedReader streamReader = new BufferedReader(new InputStreamReader(connection.getInputStream(), "UTF-8"));
+                StringBuilder responseStrBuilder = new StringBuilder();
+                String inputStr;
+                while ((inputStr = streamReader.readLine()) != null) {
+                    responseStrBuilder.append(inputStr);
+                }
+                streamReader.close();
+                String data = responseStrBuilder.toString();
+                array = new JSONArray(data);
+
+            } catch (Exception e){
+                e.printStackTrace();
             }
-        };
+            return array;
+        }
 
-        // start thread
-        background.start();
+        //updates the jsonarray in the mapactivity class, then start the activity
+        protected void onPostExecute(JSONArray array){
 
+            MapActivity.array = array;
+            Intent intent=new Intent(getBaseContext(),MapActivity.class);
+            startActivity(intent);
+        }
     }
 
     @Override
